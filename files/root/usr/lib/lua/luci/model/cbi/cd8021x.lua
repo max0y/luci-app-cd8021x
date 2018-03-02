82fd9311e62624627b1e515f88de1c74a59f742a
max0y <askmaxwork@gmail.com>
2018-03-03 00:14:47 +0800

    duplicate word fix

--- files/root/usr/lib/lua/luci/model/cbi/cd8021x.lua
+++ files/root/usr/lib/lua/luci/model/cbi/cd8021x.lua
@@ -1,6 +1,6 @@
 require("luci.sys")
 
-m = Map("cd8021x", translate("cd802.1x client"), translate("Configure wired IEEE 802.1x wired authentication using wpa_supplicant, you should change your WAN interface protocol as \"<span style=\"color:red;\">DHCP client</span>\" first. <a href=\"network/wan\">click here to change</a>"))
+m = Map("cd8021x", translate("cd802.1x client"), translate("Configure IEEE 802.1x wired authentication using wpa_supplicant, you should change your WAN interface protocol as \"<span style=\"color:red;\">DHCP client</span>\" first. <a href=\"network/wan\">click here to change</a>"))
 
 s = m:section(TypedSection, "login", "")
 s.addremove = false
