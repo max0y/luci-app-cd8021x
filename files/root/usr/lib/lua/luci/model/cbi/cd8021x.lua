9eedaf83edfd5d2caa334ae7d76fb59b620a0ca2
max0y <askmaxwork@gmail.com>
2018-04-16 23:42:46 +0800

    update package name

--- files/root/usr/lib/lua/luci/model/cbi/cd8021x.lua
+++ files/root/usr/lib/lua/luci/model/cbi/cd8021x.lua
@@ -9,7 +9,7 @@ local eap_list = {
 	"MSCHAPV2",
 }
 
-m = Map("cd8021x", translate("cd802.1x client"), 
+m = Map("cd8021x", translate("802.1x Client"), 
     translate("Configure IEEE 802.1x wired authentication, you may need to edit your WAN interface protocol as <i>DHCP client</i>  <a href=\"network\">here</a>."))
 
 s = m:section(TypedSection, "login", "")
